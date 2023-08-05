ccea12cb1a0cf7fe4799c20c1b174394fef66726
Carlos Fdez. Llamas <hello@sirikon.me>
2023-08-06 01:42:10 +0200

    Complete java manager

--- modules/cli/srkbz_jenkins/binaries/java_manager.py
+++ modules/cli/srkbz_jenkins/binaries/java_manager.py
@@ -1,9 +1,11 @@
 import platform
-from os import makedirs
+from os import makedirs, symlink
 from os.path import join
 from dataclasses import dataclass
+import tarfile
 
 import requests
+from tqdm import tqdm
 
 from srkbz_jenkins.paths.paths_provider import PathsProvider, paths_provider
 
@@ -11,9 +13,10 @@ from srkbz_jenkins.paths.paths_provider import PathsProvider, paths_provider
 @dataclass
 class _PackageInfo:
     url: str
+    home_path: str
 
 
-_adoptium_platform_mapper = {"Darwin_arm64": ("mac", "aarch64")}
+_adoptium_platform_mapper = {"Darwin_arm64": ("mac", "aarch64", "Contents/Home")}
 
 
 class JavaManager:
@@ -22,17 +25,41 @@ class JavaManager:
 
     def install(self, version: str):
         binaries_dir = self._paths_provider.get_binaries_dir()
-        installation_dir = join(binaries_dir, "java", version, "packages")
-        package_info = self._get_package_info(version)
-        makedirs(installation_dir, exist_ok=True)
 
-        downloaded = 0
-        with open(join(installation_dir, "package.tar.gz"), "wb") as f:
-            response = requests.get(package_info.url, allow_redirects=True, stream=True)
-            for data in response.iter_content(chunk_size=1024):
-                f.write(data)
-                downloaded += len(data)
-                print(downloaded)
+        version_dir = join(binaries_dir, "java", version)
+        package_dir = join(version_dir, "package")
+        package_contents_dir = join(package_dir, "contents")
+        package_file = join(package_dir, "package.tar.gz")
+        home_dir = join(version_dir, "home")
+
+        package_info = self._get_package_info(version)
+
+        makedirs(package_contents_dir, exist_ok=True)
+
+        self._download_file(
+            package_info.url,
+            package_file,
+            f"Java {version}",
+        )
+        package = tarfile.open(package_file)
+        package.extractall(package_contents_dir)
+        symlink(join(package_contents_dir, package_info.home_path), home_dir)
+
+    def _download_file(
+        self, url: str, destination: str, description: str, chunk_size: int = 1024
+    ):
+        resp = requests.get(url, allow_redirects=True, stream=True)
+        total = int(resp.headers.get("content-length", 0))
+        with open(destination, "wb") as file, tqdm(
+            desc=description,
+            total=total,
+            unit="iB",
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as bar:
+            for data in resp.iter_content(chunk_size=chunk_size):
+                size = file.write(data)
+                bar.update(size)
 
     def _get_package_info(self, version: str) -> _PackageInfo:
         adoptium_api_url = self._get_adoptium_api_url(version)
@@ -44,9 +71,16 @@ class JavaManager:
             allow_redirects=True,
         )
 
+        system_os = platform.system()
+        system_arch = platform.machine()
+        (_, _, home_path) = _adoptium_platform_mapper[f"{system_os}_{system_arch}"]
+
         for item in response.json():
             if item["binary"]["image_type"] == "jdk":
-                return _PackageInfo(url=item["binary"]["package"]["link"])
+                return _PackageInfo(
+                    url=item["binary"]["package"]["link"],
+                    home_path=item["release_name"] + "/" + home_path,
+                )
 
         raise Exception("Could not find a compatible Java package")
 
@@ -58,7 +92,7 @@ class JavaManager:
         if adoptium_platform is None:
             raise Exception(f"Unsupported platform: {system_os} {system_arch}")
 
-        (adoptium_os, adoptium_arch) = adoptium_platform
+        (adoptium_os, adoptium_arch, _) = adoptium_platform
         return f"https://api.adoptium.net/v3/assets/latest/{version}/hotspot?os={adoptium_os}&architecture={adoptium_arch}"
 
 
